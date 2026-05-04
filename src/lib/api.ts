53455833521aaf8b8e83a7b7a27bf38ebf428811
Ammad Sarfraz <asyousufzai@hotmail.com>
2026-05-04 11:11:02 +0500

    feat: add axios api client with interceptor

---
+++ src/lib/api.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = "/signin";
+    }
+
+    return Promise.reject(error);
+  }
+);
